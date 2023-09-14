709b81839ef81743e1525da5d7cd1c7e9c62eba5
soooochan <nowee018@naver.com>
2023-09-14 17:21:30 +0900

    Add: container2

--- src/pages/Introduce.js
+++ src/pages/Introduce.js
@@ -18,7 +18,8 @@ class Home extends React.Component {
         </div>
 
         <div className="container2">
-          <p>GAE4COON은 보안 전문가들로 구성되며, 클라우드 마이그레이션 이전 다계에서의 ISO 27001을 기반으로 한 보안과 도식화를 제공합니다.</p>
+          <p>GAE4COON은 보안 전문가들로 구성되며, <br/>
+          클라우드 마이그레이션 이전 단계에서 ISO 27001을 기반으로 한 보안과 도식화를 제공합니다.</p>
         </div>
 
       </div>
