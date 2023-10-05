e84ed55ee549c839893031f6ef8eab4b6bd915a4
soooochan <nowee018@naver.com>
2023-10-05 15:49:09 +0900

    Add: back and front connect

--- src/components/SelectToggle.js
+++ src/components/SelectToggle.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "../styles/SelectToggle.css";
+import axios from 'axios';
 
 const baseOptions = [
   { value: "T1", label: "Linux" },
@@ -20,6 +21,7 @@ const windowsOptions = [
 
 
 const SelectToggle = ({ uniquekey, onToggleSelect }) => {
+  
   const [toggleValues, setToggleValues] = useState([null, null, null]);
   const [nextOptions, setNextOptions] = useState([]);
 
@@ -27,84 +29,82 @@ const SelectToggle = ({ uniquekey, onToggleSelect }) => {
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
 
-  const fetchData = async () => {
+  function fetchData(){
     setLoading(true);
-    setError(null);
-    
-    try {
-      const response = await fetch("http://localhost:8080/ec2/platform", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          // 여기에 백엔드에 보내고 싶은 데이터를 추가하세요.
-          "platform":"linux",
-          "instanceType":"c6g.medium"
-        })
-      });
-  
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      const data = await response.json();
-      console.log("data", JSON.stringify(data));
-      setData(data);
-    } catch (error) {
-      setError(error);
-    } finally {
+    axios({
+      url: '/ec2/platform',
+      method: 'post',
+      data: {
+        "platform": "linux"
+      },
+      baseURL: 'http://localhost:8080',
+    })
+    .then(function (response) {
+      setData(response.data);
+      console.log("data", response.data);
+      console.log(response.data.JavaData[0].NICKNAME);
       setLoading(false);
+    })
+    .catch(function (error) {
+      console.error("Error occurred:", error);
+      setError(error);
+      setLoading(false);
+    });
+}
+
+
+
+const handleChange = (index, event) => {
+  const newValue = event.target.value;
+  const newValues = [...toggleValues];
+  newValues[index] = newValue;
+  console.log("nextValues",newValues);
+  setToggleValues(newValues);
+  onToggleSelect(newValue);
+
+  if (index === 0) {
+    if (newValue === "T1") {
+      setNextOptions(linuxOptions);
+    } else if (newValue === "T2") {
+      setNextOptions(windowsOptions);
+    } else {
+      setNextOptions([]);
     }
-  };
+  }
+};
+
+const renderToggle = (index) => (
+  <select
+    value={toggleValues[index] || ""}
+    onChange={(e) => handleChange(index, e)}
+  >
+    <option value="" disabled>Select...</option>
+    {(index === 0 ? baseOptions : nextOptions).map((option) => (
+      <option key={option.value} value={option.value}>
+        {option.label}
+      </option>
+    ))}
+  </select>
+);
+
+useEffect(() => {
+  if (uniquekey.includes('Arch_Amazon-EC2_48')) {
+    fetchData();
+  }
+}, [uniquekey]);
+
+return (
+  <div className="toggle-component">
+    {renderToggle(0)}
+    {renderToggle(1)}
+    {renderToggle(2)}
+  </div>
+);
 
 
 
-  const handleChange = (index, event) => {
-    const newValue = event.target.value;
-    const newValues = [...toggleValues];
-    newValues[index] = newValue;
-    console.log("nextValues",newValues);
-    setToggleValues(newValues);
-    onToggleSelect(newValue);
 
-    if (index === 0) {
-      if (newValue === "T1") {
-        setNextOptions(linuxOptions);
-      } else if (newValue === "T2") {
-        setNextOptions(windowsOptions);
-      } else {
-        setNextOptions([]);
-      }
-    }
-  };
-
-  const renderToggle = (index) => (
-    <select
-      value={toggleValues[index] || ""}
-      onChange={(e) => handleChange(index, e)}
-    >
-      <option value="" disabled>Select...</option>
-      {(index === 0 ? baseOptions : nextOptions).map((option) => (
-        <option key={option.value} value={option.value}>
-          {option.label}
-        </option>
-      ))}
-    </select>
-  );
-
-  useEffect(() => {
-    if (uniquekey.includes('Arch_Amazon-EC2_48')) {
-      fetchData();
-    }
-  }, [uniquekey]);
-
-  return (
-    <div className="toggle-component">
-      {renderToggle(0)}
-      {renderToggle(1)}
-      {renderToggle(2)}
-    </div>
-  );
+  
 };
 
 export default React.memo(SelectToggle);
