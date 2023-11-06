3245d0e6531f06f9bc7c6434593320f777429999
naindae <wkd5860@naver.com>
2023-11-06 16:38:10 +0900

    Feat: prevent mutiple Rehost

--- src/pages/Button.js
+++ src/pages/Button.js
@@ -71,8 +71,14 @@ const Button = ({
     }
   };
 
+  let rehostCount=0;
+
   const handleLoad = async () => {
     try {
+      if (rehostCount >= 1) {
+        alert('!!!!!!!!!!Cloud를 Rehost 할 수 없습니다!!!!!!!!!');
+        return;
+      }
       ////console.log("modelmodel",JSON.stringify(diagram.model));
 
       const jsonString = diagram.model.toJson();
@@ -83,6 +89,7 @@ const Button = ({
       const Jdata = response.data.result;
 
       diagram.model = go.Model.fromJson(Jdata);
+      rehostCount++;
     } catch (error) {
       console.error("rehost error: ", error);
     }
