091e6c88126c54fcd72d3734749661ebb252eee5
ssoxong <ssoxong@naver.com>
2023-11-22 18:02:00 +0900

    feat: node text

--- json.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# JSON 파일의 경로를 설정합니다. 예: /path/to/your/file.json
-json_file="D:\cloud_front\Node.json"
-
-# 'jq'를 사용하여 'text' 필드를 추가합니다.
-jq 'map(. + {"text": .key})' $json_file > temp.json && mv temp.json $json_file
