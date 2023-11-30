b794728881af1d6c2e3d13fd252c95c76c880eb6
leesumin <66521935+hellokms8828@users.noreply.github.com>
2023-11-30 16:17:13 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
- echo "> FE 배포"
- rm -rf node_modules
- rm package-lock.json
- npm install
- nohup npm start &
+echo "> FE 배포"
+rm -rf node_modules
+rm package-lock.json
+npm install
+nohup npm start &
