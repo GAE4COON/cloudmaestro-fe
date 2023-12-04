98a0bc9983c34d4c39d71080f1bea2c0792af4cf
naindae <98092591+naindae@users.noreply.github.com>
2023-12-04 16:24:37 +0900

    Update index.js

--- src/apis/index.js
+++ src/apis/index.js
@@ -6,7 +6,7 @@ let REFRESH_TOKEN = localStorage.getItem("refreshToken");
 
 //proxy: "http://localhost:8080", path:"/api/v1/users-api/",
 const api = axios.create({
-  baseURL: "https://43.201.196.128:8080",
+  baseURL: "https://cloudmaestroback.site",
   headers: {
     "Content-type": "application/json",
     Authorization: `${TOKEN_TYPE} ${ACCESS_TOKEN}`,
