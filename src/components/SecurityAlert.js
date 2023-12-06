017e8293c698cd24ec4e1c6f6c1ad27fe90b71bb
soooochan <nowee018@naver.com>
2023-12-06 18:29:56 +0900

    Merge Before

--- src/components/SecurityAlert.js
+++ src/components/SecurityAlert.js
@@ -6,7 +6,7 @@ export async function handleSecurity(e, diagram, setAlertMessage) {
   if (e.isTransactionFinished) {
     const jsonString = e.model.toIncrementalJson(e);
     const data = JSON.parse(jsonString);
-  
+    checkForLog(diagram, setAlertMessage)
 
     if (data.modifiedLinkData) {
       // TODO
@@ -22,7 +22,6 @@ export async function handleSecurity(e, diagram, setAlertMessage) {
 
     // Handling node modifications separately
     if (data.modifiedNodeData) {
-      console.log("제발 호출되라");
       for (let i = 0; i < data.modifiedNodeData.length; i++) {
         if (data.modifiedNodeData[i].text === "QuickSight" ||
             data.modifiedNodeData[i].text ===  "OpenSearch Service" ||
@@ -71,20 +70,45 @@ export function checkForLog(diagram, setAlertMessage) {
     const hasOpenSearch = diagram.model.nodeDataArray.some(node => node.text === "OpenSearch Service");
     const hasAthena = diagram.model.nodeDataArray.some(node => node.text === "Athena");
     const hasS3 =  diagram.model.nodeDataArray.some(node => node.text === "S3");
-    const backUpGuide = hasQuickSightNode + "정보, 소프트웨어 및 시스템의 백업 사본은 합의된 백업 주제의 정책에 따라 유지되고 정기적으로 테스트되어야 함";
     console.log("hasQuickSIghtNode" , hasQuickSightNode);
     console.log("hellohihi");
-    if (!hasQuickSightNode && !hasOpenSearch && !hasAthena && !hasS3) {
-      setAlertMessage(prevDiagramCheck => {
-        const isDuplicate = prevDiagramCheck.some(item => item.message === backUpGuide);
-        const newMessage = { key: Date.now(), message: backUpGuide };
+    const message = {
+      key: Date.now().toString(), // Unique key for each message
+      hasQuickmessage: " QuickSight가 암호화되지 않을 시, 무단 접근 및 변조 등을 통한 보험 위험이 존재할 수 있습니다.",
+      hasOpenSearch:" OpenSearch 암호화되지 않을 시, 무단 접근 및 변조 등을 통한 보험 위험이 존재할 수 있습니다.",
+      hasAthena:" Athena가 암호화되지 않을 시, 무단 접근 및 변조 등을 통한 보험 위험이 존재할 수 있습니다.",
+      hasS3:" S3가 암호화되지 않을 시, 무단 접근 및 변조 등을 통한 보험 위험이 존재할 수 있습니다."
 
-        if (!isDuplicate) {
-          return [...prevDiagramCheck, newMessage];
-        } else {
-          return prevDiagramCheck.map(item => item.message === backUpGuide ? { ...item, ...newMessage } : item);
-        }
+    
+    };
+    if (hasQuickSightNode) {
+      setAlertMessage({
+        key: Date.now(), // 현재 타임스탬프를 key로 사용
+        message: message.hasQuickmessage,
+        tag: "Info",
       });
     }
+    if (hasOpenSearch) {
+      setAlertMessage({
+        key: Date.now()+1, // 현재 타임스탬프를 key로 사용
+        message: message.hasOpenSearch,
+        tag: "Info",
+      });
+    }
+    if (hasAthena) {
+      setAlertMessage({
+        key: Date.now()+2, // 현재 타임스탬프를 key로 사용
+        message: message.hasAthena,
+        tag: "Info",
+      });
+    }
+    if (hasS3) {
+      setAlertMessage({
+        key: Date.now()+3, // 현재 타임스탬프를 key로 사용
+        message: message.hasS3,
+        tag: "Info",
+      });
+    }
+    
   }
 }
