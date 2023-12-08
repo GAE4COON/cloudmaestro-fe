80f7f21313614355112d56ccb3ebf084c3381ddb
ssoxong <ssoxong@naver.com>
2023-12-08 16:09:32 +0900

    style: nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,8 @@ import { useAuth } from "./../utils/auth/authContext";
 import { useLocation, Link } from "react-router-dom";
 import jwtDecode from "jwt-decode";
 import { Layout, Menu, Dropdown, Button, Avatar } from 'antd';
-import { UserOutlined, LogoutOutlined } from '@ant-design/icons';
-import { DownOutlined } from "@ant-design/icons";
+import { LogoutOutlined } from '@ant-design/icons';
+import styled from "styled-components";
 
 const { Header } = Layout;
 
@@ -24,6 +24,7 @@ const Navbar = () => {
       }
     } else {
       setUser(null);
+      localStorage.clear();
     }
   }, [ACCESS_TOKEN, setUser]);
 
@@ -31,6 +32,11 @@ const Navbar = () => {
     setUser(null);
     localStorage.clear();
     window.location.href = "/home";
+  };
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownVisible(!isDropdownVisible);
   };
 
   const userDropdownMenu = (
@@ -45,53 +51,136 @@ const Navbar = () => {
   );
   const homeMenu = (
     <Menu>
-      <Menu.Item key="home">
-        <Link to="/home">Home</Link>
-      </Menu.Item>
       <Menu.Item key="autodraw">
-        <Link to="/home/autodraw">AutoDraw</Link>
+        <Link to="/home/auto">AutoDraw</Link>
       </Menu.Item>
     </Menu>
   );
   
 
   return (
-    <Header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-      <Link to="/">
-        <img src="/assets/img/logo.png" alt="logo" style={{ height: '64px' }} />
-      </Link>
-      <Menu theme="dark" mode="horizontal" selectedKeys={[location.pathname]} style={{ flex: 1 }}>
-      <Dropdown overlay={homeMenu} trigger={['hover']}>
-          <Link to="/home" className="ant-dropdown-link" onClick={e => e.preventDefault()}>
-            Home
-          </Link>
-        </Dropdown>
+    <NavStyled style={{ fontFamily: "Noto Sans KR", backgroundColor:"white", display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+      <NavLink to="/">
+        <img src="/assets/img/logo.png" alt="logo" />
+      </NavLink>
+      <Menu mode="horizontal" selectedKeys={[location.pathname]} style={{ flex: 1 }}>
+      <Menu.Item key="/home">
+          <Dropdown
+            overlay={homeMenu}
+            trigger={['hover']}
+            visible={isDropdownVisible}
+            onVisibleChange={setIsDropdownVisible}
+          >
+            <NavLink to="/home" className="ant-dropdown-link" onClick={toggleDropdown}>
+              Home
+            </NavLink>
+          </Dropdown>
+        </Menu.Item>
         <Menu.Item key="/draw">
-          <Link to="/draw">Draw</Link>
+          <NavLink to="/draw">Draw</NavLink>
         </Menu.Item>
         <Menu.Item key="/about">
-          <Link to="/about">Introduce</Link>
+          <NavLink to="/about">Introduce</NavLink>
         </Menu.Item>
       </Menu>
       {user ? (
-        <Dropdown overlay={userDropdownMenu} trigger={['click']}>
+        <Dropdown overlay={userDropdownMenu} trigger={['hover']}>
           <a onClick={(e) => e.preventDefault()}>
-            <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
-            {user.name} <DownOutlined />
+            <Username>{user.name}님</Username> 
           </a>
         </Dropdown>
       ) : (
         <Button.Group>
-          <Button type="primary">
-            <Link to="/sign-in">로그인</Link>
-          </Button>
-          <Button>
-            <Link to="/sign-up">회원가입</Link>
-          </Button>
+            <NavBtnLink to="/sign-in">로그인</NavBtnLink>
+            <NavBtnLink to="/sign-up">회원가입</NavBtnLink>
         </Button.Group>
       )}
-    </Header>
+    </NavStyled>
   );
 };
 
 export default Navbar;
+
+const Username = styled.div`
+  width: auto;
+  height: auto;
+  border-radius: 4px;
+  padding: 5px 12px;
+  color: #000000;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  // margin-left: 5px;
+  align-items: center;
+  font-size: 16px;
+`;
+const NavBtnLink = styled(Link)`
+  width: 65px;
+  border-radius: 4px;
+  /* background: #fff; */
+  padding: 5px 12px;
+  color: #3064d6;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  // margin-left: 5px;
+  align-items: center;
+  font-size: 16px;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    color: #3064d6;
+    font-weight: bold;
+  }
+`;
+
+
+const NavLink = styled(Link)`
+  position: relative;
+  color: #000;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  font-size: 17px;
+  height: 100%;
+  cursor: pointer;
+
+  margin-right: 20px; // adjust as needed
+  &:last-child {
+    margin-right: 0; // so the last item doesn't have extra margin
+  }
+
+  & img {
+    width: 40px;
+    height: auto;
+    margin-right: 50px;
+  }
+
+  &.active {
+    color: #3064d6;
+  }
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    color: #3064d6;
+  }
+`;
+
+const NavStyled = styled(Header)`
+  background: #fff;
+  width: 100%;
+  /* height: 35px; */
+  display: flex;
+  justify-content: space-around;
+  padding: 0.5rem calc(0.125 * ((100vw - 1000px) / 2));
+  z-index: 999;
+  position: relative;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  font-family: "Noto Sans KR", sans-serif !important;
+
+
+`;
