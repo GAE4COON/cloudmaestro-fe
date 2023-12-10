f18c95f1cdd6cde4498eaf24ac4f760d6257763b
ssoxong <ssoxong@naver.com>
2023-12-11 01:40:04 +0900

    style: mydiagram

--- src/pages/MyDiagram.js
+++ src/pages/MyDiagram.js
@@ -7,11 +7,11 @@ import { DownOutlined } from "@ant-design/icons";
 import "../styles/App.css";
 import { Space, Dropdown, Button } from "antd";
 import { Menu } from "antd";
-import { CloseButton } from "react-bootstrap";
 import { useAuth } from "../utils/auth/authContext";
 import jwtDecode from "jwt-decode";
 import { CloseOutlined } from "@ant-design/icons";
-import { Avatar, Card } from "antd";
+import { Avatar, Card, Tooltip } from "antd";
+
 
 import styled from "styled-components";
 import { getDiagramData, myNetworkDB, deleteDiagramData } from "../apis/myPage";
@@ -138,32 +138,33 @@ const MyArchitecture = () => {
                           okText="Yes"
                           placement="right"
                         >
-                          <CloseOutlined
+                          {/* <CloseOutlined
                             style={{
                               position: "absolute",
                               top: "10px",
                               right: "10px",
                             }}
-                          />{" "}
+                          />{" "} */}
+
+                          <Button style={{
+                            position: "absolute",
+                            top: "5px",
+                            right: "5px",
+                          }}
+                            type="text"
+                            shape="circle" icon={<CloseOutlined />} />
                         </Popconfirm>
 
-                        <img
+                        <CloudInstanceImg
                           onClick={() =>
                             handleCloudInstance(instance.key, "/draw")
                           }
                           alt="diagram_img"
                           src={`https://cm-user-file.s3.ap-northeast-2.amazonaws.com/${instance.title}_${user.sub}.png`}
-                          style={{
-                            marginTop: "20px",
-                            width: "100%",
-                            height: "40%",
-                            objectFit: "contain",
-                            cursor: "pointer",
-                            // borderRadius: "5px",
-                            // boxShadow: "1px 1px 1px 1px rgb(235, 235, 235)",
-                          }}
+
                         />
-                        <StyledInstanceTitle>
+                        <CloudInstanceETC>
+                          <StyledInstanceTitle>
                           {instance.title}
                         </StyledInstanceTitle>
 
@@ -192,6 +193,8 @@ const MyArchitecture = () => {
                             </StyledButton>
                           </Dropdown>
                         </ButtonContainer>
+                        </CloudInstanceETC>
+
                       </CloudInstance>
                     );
                   })}
@@ -217,19 +220,45 @@ const MyArchitecture = () => {
 };
 
 export default MyArchitecture;
+const CloudInstanceETC = styled.div`
+  display: flex;
+  flex-direction: row; // 가로 방향으로 요소를 배열
+  /* align-items: center; // 세로 축 중앙 정렬 */
+  justify-content: space-between; // 요소 사이의 공간을 균등하게 배분
+  /* width: 100%; // 전체 너비를 사용 */
+  flex-wrap: wrap; // 필요한 경우 요소를 다음 줄로 넘김
+  padding: 20px;
+`;
+
+const CloudInstanceImg = styled.img`
+  /* margin-top: 30px; */
+  width: 100%;
+  height: 60%;
+  object-fit: contain;
+  cursor: "pointer";
+  border-radius: 5px 5px 0px 0px;
+  border: 1px solid #e8e8e8;
+`
+
 const CloudInstance = styled.div`
   width: 30%; // Adjust the width to fit 3 instances per row
-  height: 320px;
-  border: 1px solid gray;
+  height: 280px;
+  /* border: 1px solid gray; */
   border-radius: 5px;
+  flex-direction: column; // 화면이 작을 때 세로 방향으로 쌓음
+  @media (min-width: 600px) {
+    flex-direction: row; // 화면이 600px 이상일 때 가로 방향으로 배치
+  }
   box-shadow: 1px 1px 1px 1px rgb(235, 235, 235);
   position: relative;
-  margin-top: 10px; // Keep right margin
+  margin-left: 5px;
+  margin-right: 5px;
+  margin-bottom: 20px;
 `;
 
 const CloudInstanceRow = styled.div`
-  margin-left: 10px; // Keep left margin
-  margin-right: 10px; // Keep right margin
+  margin-left: 20px; // Keep left margin
+  margin-right: 20px; // Keep right margin
   display: flex;
   justify-content: flex-start; // Align items to the start of the row
   flex-wrap: wrap; // Wrap items to the next line if they overflow
@@ -237,43 +266,42 @@ const CloudInstanceRow = styled.div`
   gap: 10px; // You can use gap property to maintain consistent spacing
 `;
 
-const DeleteInstanceButton = styled.button`
-  font-size: 15px;
-  font-weight: 500;
-  position: absolute; /* 절대적 위치 지정 */
-  top: 0px; /* 상단에서의 위치 */
-  right: 3px;
-  background-color: transparent;
-`;
-
 const StyledButton = styled(Button)`
   min-width: 100px;
-  margin: 5px;
+  margin-bottom: 5px;
   color: white;
   font-weight: 500;
   display: flex;
   justify-content: space-between;
   border-radius: 20px;
-
+  font-family: "Noto Sans KR", sans-serif !important;
   /* text-align: left; */
 `;
 
 const ButtonContainer = styled.div`
-  align-items: end;
+
+  /* align-items: end; */
   display: flex;
   flex-direction: column;
+  align-items: flex-end; // 오른쪽 정렬
+  flex-grow: 0;
+  flex:0;
 `;
 
 const StyledSideMenuTitle = styled.div`
   font-family: "Noto Sans KR", sans-serif !important;
   font-weight: 500;
   font-size: 20px;
+  margin-top: 30px;
+
 `;
 
 const StyledInstanceTitle = styled.div`
   font-family: "Noto Sans KR", sans-serif !important;
-  /* font-weight: 500; */
-  /* font-size: 20px; */
   text-align: left;
-  margin: 20px;
+  flex-grow: 1; 
+  flex: 1;
+  white-space: nowrap; 
+  overflow: hidden; 
+  text-overflow: ellipsis;
 `;
